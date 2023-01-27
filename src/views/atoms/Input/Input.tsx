1bf1fc91d1e7bb61ab415008ac65f1f133597e77
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-01-27 01:21:25 +0000

    update: added FONT, extended tailwindcss config to have primary color and added some more styling for the modal

--- src/views/atoms/Input/Input.tsx
+++ src/views/atoms/Input/Input.tsx
@@ -24,7 +24,7 @@ const Input: React.FC<InputProps> = (props) => {
 
     return (
         <input 
-            className="block w-full appearance-none rounded-[3px] border border-gray-300 px-3 py-2 placeholder-gray-800 text-black shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            className="block w-full placeholder:text-[#D4D4D4] appearance-none rounded-[3px] border border-gray-300 px-3 py-2 placeholder-gray-800 text-black shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             id={id}
             name={name}
             type={type}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,45 @@
+function withOpacity(variableName) {
+    return ({ opacityValue }) => {
+        if (opacityValue !== undefined) {
+            return `rgba(var(${variableName}), ${opacityValue})`
+        }
+        return `rgb(var(${variableName}))`
+    }
+}
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  safelist: [],
-  theme: {
-      // extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    safelist: [],
+    theme: {
+        extend: {
+            colors: {
+                skin: {
+                    primary: {
+                        "DEFAULT": "#000000",
+                    }
+                }
+            },
+            textColor: {
+                skin: {
+                    // base: withOpacity('--color-text-base'),
+                    // leading: withOpacity('--color-text-leading'),
+                    // muted: withOpacity('--color-text-muted'),
+                    inverted: '',
+                }
+              },
+              backgroundColor: {
+                skin: {
+                    // base: withOpacity('--color-bg-base')
+                }
+            },
+            lineHeight: {
+                "2" : "0.5rem"
+            }
+        },
+    },
+    plugins: [],
 }
